2611e10642a0eebe53ca894d529a5aaf23dfd046
ravenusmc <mcuddy77@gmail.com>
2016-07-11 14:48:56 -0400

    film page done-at basic level

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,11 @@
 class SitesController < ApplicationController
 
   def film 
+    response = HTTParty.get("http://swapi.co/api/films/#{params[:number]}")
+    @title = response['title']
+    @director = response['director']
+    @episode_id = response['episode_id']
+    @producer = response['producer']
+    @release_date = response['release_date']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'film' => 'sites#film'
+
+  post 'film' => 'sites#film'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
